12433783d5b5d76ecc48a0284b20103442c8c5bd
Corvus228 <mrfireplle2003@gmail.com>
2023-07-06 20:49:18 +0300

    Player

--- Methamphetamine/Assets/SlimeJump.cs
+++ Methamphetamine/Assets/SlimeJump.cs
@@ -7,7 +7,13 @@ public class SlimeJump : MonoBehaviour
 {
     public float JumpSpeed = 10;
     public Rigidbody2D myRigidbody;
+<<<<<<< Updated upstream
     public bool isOnGround;
+=======
+
+    [Header("Player Animation Settings")]
+    public Animator animator;
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +23,15 @@ public class SlimeJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+=======
+        animator.SetFloat("Vertical Move", Mathf.Abs(myRigidbody.velocity.y));
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(myRigidbody.velocity.y) < 0.01)
+>>>>>>> Stashed changes
         {
             myRigidbody.velocity = Vector2.up * JumpSpeed;
-            isOnGround = false;
         }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        isOnGround = true;
     }
 }
