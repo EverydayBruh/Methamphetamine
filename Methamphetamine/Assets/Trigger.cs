65bbe31e86056cf10ec63b162efacff5ba9b7d7b
AirDragon0 <artem20060boev@gmail.com>
2023-07-06 23:32:45 +0300

    Sound
    Xyi

---
+++ Methamphetamine/Assets/Trigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<AudioSource>();
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
